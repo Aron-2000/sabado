6cc950e38a79d879132dc4b08d85363662120c01
Aron-2000 <aron.habbo123@gmail.com>
2023-09-23 12:43:21 -0300

    trabajo 1

---
+++ 1-1 Suma/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b,c=0;
+	a=0;
+	b=0;
+	c=0;
+	cout<<"\n Ingrese un numero: "; 
+	cin>>a;
+	cout<<"\n Ingrese otro numero: ";
+	cin>>b;
+	c=a+b;
+	cout<<"\n La Suma Es: "<<c;
+	
+	return 0;
+}
